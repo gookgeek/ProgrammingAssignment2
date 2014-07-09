068dc598d474a6e18f174200d257d79f6e75ee9a
Anthony Nguyen <a.anthony.nguyen@gmail.com>
2014-07-09 14:32:35 -0500

    submission of ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Written on July 9, 2014
+## R Programming on Coursera
 
-## Write a short comment describing this function
+## These two functions cache an inverted matrix in order to same time and
+## memory. Inputed matrices are assumed to be invertible.
+
+## This is an extremely simple programming assignment that is closely
+## modelled by the example for caching the mean of a vector. The example
+## utilizes a vector, but the code is written in such a way that a vector
+## can be easily replaced by a matrix. Then, rather than the mean function,
+## I utilized the solve function. Otherwise, the program below and the example
+## are identical program.
+
+## The first function, makeCacheMatrix, creates a list of functions that
+## sets the matrix, gets the matrix, sets the inverted matrix, and gets the
+## inverted matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  
+  set <- function(y) {
+    
+    x <<- y
+    m <<- NULL
+    
+  }
+  
+  get <- function() x
+  
+  setsolve <- function(solve) m <<- solve
+  
+  getsolve <- function() m
+  
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
+  
 }
 
-
-## Write a short comment describing this function
+## The second function inverts the matrix created by makeCacheMatrix.
+## If the matrix has already been inverted, the functions gets it from
+## the cache. If not, then it inverts the matrix and sets it into cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  m <- x$getsolve()
+  
+  if(!is.null(m)) {
+    
+    message("getting cached data")
+    return(m)
+    
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+
+  m
+  
 }
